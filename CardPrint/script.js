fb3d475be44db6624556a2bc3c3f7799434e4c6c
Utkarsh Sharma <83834666+utkarshofficial@users.noreply.github.com>
2022-01-17 17:46:12 +0530

    Create script.js

---
+++ CardPrint/script.js
@@ -0,0 +1,47 @@
+// ! margin between front and back resizer
+var mar = 0;
+
+function rightMargin(check) {
+    if (check && mar < 10) {
+        mar++;
+    } else if (!check && mar > 0) {
+        mar--;
+    }
+    document.getElementById("marginTime").style.marginRight = mar + "px";
+    console.log(mar);
+}
+
+var first = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('paste', function(evt) {
+        var clipboardItems = evt.clipboardData.items;
+        var items = [].slice.call(clipboardItems).filter(function(item) {
+            // Filter the image items only
+            return item.type.indexOf('image') !== -1;
+        });
+        if (items.length === 0) {
+            return;
+        }
+        var item = items[0];
+        imageData = item.getAsFile();
+        if (first === 0) {
+            document.getElementById("frontImg").src = URL.createObjectURL(imageData);
+            first = 1;
+        } else if (first === 1) {
+            document.getElementById("backImg").src = URL.createObjectURL(imageData);
+            first = 0
+        }
+    });
+});
+
+// ! border function
+function checkBorder() {
+    var imgS = document.getElementsByClassName("imgSize");
+    if (document.getElementById("rounded").checked) {
+        imgS[0].style.borderRadius = "10px";
+        imgS[1].style.borderRadius = "10px";
+    } else {
+        imgS[0].style.borderRadius = "";
+        imgS[1].style.borderRadius = "";
+    }
+}
